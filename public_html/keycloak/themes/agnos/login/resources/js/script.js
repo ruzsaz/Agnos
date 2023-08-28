c2f438b41ac20beda88b3bfb2859104444120133
ruzsaz <ruzsaz@gmail.com>
2023-08-28 21:17:15 +0200

    Keycloak login screen bug fixed

--- public_html/keycloak/themes/agnos/login/resources/js/script.js
+++ public_html/keycloak/themes/agnos/login/resources/js/script.js
@@ -8,7 +8,7 @@ var getCssFile = function () {
 	return username;
     }
     return getCookie("loginCss");
-}
+};
 
 var changeCSS = function (cssFile) {
     var newLink = document.createElement("link");
@@ -26,11 +26,21 @@ function setCookie(cookieName, cookieValue) {
 function getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
+  if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
-window.onload = function() {
-    const cssToUse = getCssFile();
-    changeCSS("/resources/atk2l/login/agnos/" + cssToUse);
-    document.body.setAttribute("style", "opacity: 1");
+function getBasePath() {
+    var scripts = document.getElementsByTagName("script");
+    var path = scripts[scripts.length - 1].src;
+    return /\/resources.*agnos\//g.exec(path)[0];
 }
+
+const basePath = getBasePath();
+
+window.onload = function() {    
+    const cssToUse = getCssFile();
+    if (cssToUse !== undefined) {
+        changeCSS("/resources/atk2l/login/agnos/" + cssToUse);
+    }
+    document.body.setAttribute("style", "opacity: 1");
+};
