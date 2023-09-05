cc666fe967c66b0e180ccb0d77b61537c702848d
ruzsaz <ruzsaz@gmail.com>
2023-09-05 11:11:43 +0200

    Local and prod separation finished

--- public_html/localSettings.js
+++ public_html/localSettings.js
@@ -10,14 +10,14 @@
 //var baseUrl = "http://agnos.hu/AgnosReportingServer";
 //var baseUrl = "http://10.64.4.21:7979/AgnosReportingServer";  // Zsolt gépe
 //var baseUrl = "http://agnos:9091/ars";
-var baseUrl = "http://192.168.1.2:9091/ars";
-//const baseUrl = "http://localhost:9091/ars";
+//var baseUrl = "http://192.168.1.2:9091/ars";
+const baseUrl = "http://localhost:9091/ars";
 //const baseUrl = "http://localhost:7979/ars";
 
 var agnosConfig = {};
 //agnosConfig.keycloakSettings = {url: 'http://localhost:8080', realm: 'AgnosRealm', clientId: 'agnos'};
-//agnosConfig.keycloakSettings = {url: 'http://localhost:8082/auth', realm: 'AgnosRealm', clientId: 'agnos'};
-agnosConfig.keycloakSettings = {url: 'http://192.168.1.2:8082/auth', realm: 'AgnosRealm', clientId: 'agnos'};
+agnosConfig.keycloakSettings = {url: 'http://localhost:8082/auth', realm: 'AgnosRealm', clientId: 'agnos'};
+//agnosConfig.keycloakSettings = {url: 'http://192.168.1.2:8082/auth', realm: 'AgnosRealm', clientId: 'agnos'};
 agnosConfig.url = {
     auth: baseUrl + "/auth/login",
     superMeta: baseUrl + "/meta/cubes",
